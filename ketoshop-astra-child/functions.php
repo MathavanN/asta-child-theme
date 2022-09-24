a7f7432439f509add1eff822e7205946b31eaf1a
Mathavan <math08avan@gmail.com>
2022-09-24 11:44:42 +0800

    Fix(Ketoshop) : theme modification updated

--- ketoshop-astra-child/functions.php
+++ ketoshop-astra-child/functions.php
@@ -22,4 +22,30 @@ function child_enqueue_styles() {
 
 }
 
+/**
+ * Shop page - Short Description
+ */
+if ( ! function_exists( 'astra_woo_shop_product_short_description' ) ) :
+	/**
+	 * Product short description
+	 *
+	 * @hooked woocommerce_after_shop_loop_item
+	 *
+	 * @since 1.1.0
+	 */
+	function astra_woo_shop_product_short_description() {
+		?>
+		<?php if ( has_excerpt() ) { ?>
+		<div class="ast-woo-shop-product-description">
+		    <p><?php echo wp_trim_words(get_the_excerpt(), 10); ?></p>
+		</div>
+	<?php } ?>
+		<?php
+	}
+endif;
+
+
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+add_theme_support( 'woocommerce' );
+
+
